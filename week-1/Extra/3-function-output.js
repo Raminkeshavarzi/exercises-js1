2d37981669afdd629c92807a5183f60ba4d53ca1
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-08-15 18:28:47 +0200

    fix the code

--- week-1/Extra/3-function-output.js
+++ week-1/Extra/3-function-output.js
@@ -1,16 +1,20 @@
 // Add comments to explain what this function does. You're meant to use Google!
+// This code generate random number from 0 to 9
 function getNumber() {
-  return Math.random() * 10;
+	return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+// this code takes two inputs and then by passing them through this fubction concatinate them into one string
 function s(w1, w2) {
-  return w1.concat(w2);
+	return w1.concat(w2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
-  // Write the body of this function to concatenate three words together.
-  // Look at the test case below to understand what this function is expected to return.
+	// Write the body of this function to concatenate three words together.
+	// Look at the test case below to understand what this function is expected to return.
+
+	return `${firstWord} ${secondWord} ${thirdWord}`;
 }
 
 /* ======= TESTS - DO NOT MODIFY =====
@@ -21,28 +25,16 @@ To run these tests type `node 3-function-output` into your terminal
 const util = require('util');
 
 function test(test_name, actual, expected) {
-    let status;
-    if (actual === expected) {
-        status = "PASSED";
-    } else {
-        status = `FAILED: expected: ${util.inspect(expected)} but your function returned: ${util.inspect(actual)}`;
-    }
+	let status;
+	if (actual === expected) {
+		status = 'PASSED';
+	} else {
+		status = `FAILED: expected: ${util.inspect(expected)} but your function returned: ${util.inspect(actual)}`;
+	}
 
-    console.log(`${test_name}: ${status}`);
+	console.log(`${test_name}: ${status}`);
 }
 
-test(
-  "concatenate function - case 1 works",
-  concatenate('code', 'your', 'future'),
-  "code your future"
-);
-test(
-  "concatenate function - case 2 works",
-  concatenate('I', 'like', 'pizza'),
-  "I like pizza"
-);
-test(
-  "concatenate function - case 3 works",
-  concatenate('I', 'am', 13),
-  "I am 13"
-);
+test('concatenate function - case 1 works', concatenate('code', 'your', 'future'), 'code your future');
+test('concatenate function - case 2 works', concatenate('I', 'like', 'pizza'), 'I like pizza');
+test('concatenate function - case 3 works', concatenate('I', 'am', 13), 'I am 13');
