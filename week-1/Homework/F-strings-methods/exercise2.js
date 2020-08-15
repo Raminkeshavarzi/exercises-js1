f03f973094ebd291aca49fac0f275e1de324f4b2
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-08-15 17:38:58 +0200

    add changes

--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,3 +1,3 @@
-const name = " Daniel  ";
-
-console.log(message);
+const name = ' Daniel  ';
+let nameWithoutSpace = name.trim();
+console.log(`My name is ${nameWithoutSpace} and my name is ${nameWithoutSpace.length} characters long`);
