47a84dfe0d8885ae4b271b8e0cf57e7529080679
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-08-15 18:21:19 +0200

    fix the code

--- week-1/Extra/2-logic-error.js
+++ week-1/Extra/2-logic-error.js
@@ -1,16 +1,15 @@
 // The syntax for this function is valid but it has an error, find it and fix it.
-
+let wordtrim;
 function trimWord(word) {
-  return wordtrim();
+	return `${word.trim()}`;
 }
 
 function getWordLength(word) {
-  return "word".length()
+	return word.length;
 }
 
 function multiply(a, b, c) {
-  a * b * c;
-  return;
+	return a * b * c;
 }
 
 /* ======= TESTS - DO NOT MODIFY =====
@@ -22,16 +21,16 @@ To run these tests type `node 2-logic-error` into your terminal
 const util = require('util');
 
 function test(test_name, actual, expected) {
-    let status;
-    if (actual === expected) {
-        status = "PASSED";
-    } else {
-        status = `FAILED: expected: ${util.inspect(expected)} but your function returned: ${util.inspect(actual)}`;
-    }
+	let status;
+	if (actual === expected) {
+		status = 'PASSED';
+	} else {
+		status = `FAILED: expected: ${util.inspect(expected)} but your function returned: ${util.inspect(actual)}`;
+	}
 
-    console.log(`${test_name}: ${status}`);
+	console.log(`${test_name}: ${status}`);
 }
 
-test("fixed trimWord function", trimWord("  CodeYourFuture "), "CodeYourFuture");
-test("fixed wordLength function", getWordLength("A wild sentence appeared!"), 25);
-test("fixed multiply function", multiply(2, 3, 6), 36);
+test('fixed trimWord function', trimWord('  CodeYourFuture '), 'CodeYourFuture');
+test('fixed wordLength function', getWordLength('A wild sentence appeared!'), 25);
+test('fixed multiply function', multiply(2, 3, 6), 36);
