2ffc9e0b10d11ac22d764a2307e652b1beb0ead0
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-08-15 18:49:53 +0200

    fix the code

--- week-1/Extra/5-currency-conversion.js
+++ week-1/Extra/5-currency-conversion.js
@@ -5,7 +5,9 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(amountOfCurr) {
+	return 1.4 * amountOfCurr;
+}
 
 /*
   CURRENCY FORMATTING
@@ -15,7 +17,11 @@ function convertToUSD() {}
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+function convertToBRL(amountOfBrl) {
+	const totalExchange = 5.7 * amountOfBrl;
+	const transactions = 5.7 * amountOfBrl * 0.01;
+	return totalExchange - transactions;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
@@ -26,15 +32,15 @@ To run these tests type `node 1-currency-conversion` into your terminal
 const util = require('util');
 
 function test(test_name, actual, expected) {
-    let status;
-    if (actual === expected) {
-        status = "PASSED";
-    } else {
-        status = `FAILED: expected: ${util.inspect(expected)} but your function returned: ${util.inspect(actual)}`;
-    }
+	let status;
+	if (actual === expected) {
+		status = 'PASSED';
+	} else {
+		status = `FAILED: expected: ${util.inspect(expected)} but your function returned: ${util.inspect(actual)}`;
+	}
 
-    console.log(`${test_name}: ${status}`);
+	console.log(`${test_name}: ${status}`);
 }
 
-test("convertToUSD function works", convertToUSD(32), 44.8);
-test("convertToBRL function works", convertToBRL(30), 169.29);
+test('convertToUSD function works', convertToUSD(32), 44.8);
+test('convertToBRL function works', convertToBRL(30), 169.29);
