137fc11749d6eccc3216fa0676ef11ef994171d2
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-08-19 13:06:06 +0200

    add file to the js

--- week-2/InClass/exercise-J.js
+++ week-2/InClass/exercise-J.js
@@ -1,6 +1,10 @@
-function secondMatchesAmy(array) {
-  if ( ) {
-    return "Second index matched!";
-  }
-  return "Second index not matched";
+const arr = [ 'hello', 'navy', 'Amy', 'Army' ];
+
+function secondMatchesAmy(arr) {
+	if (arr[2] === 'Amy') {
+		return 'Second index matched!';
+	}
+	return 'Second index not matched';
 }
+
+console.log(secondMatchesAmy(arr));
