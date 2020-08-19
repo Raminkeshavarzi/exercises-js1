f6f29e80551e8843176fbf3c4bd63be4232de882
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-08-19 12:35:56 +0200

    added the code

--- week-2/InClass/exercise-G.js
+++ week-2/InClass/exercise-G.js
@@ -1,4 +1,6 @@
-var apolloCountdownMessage = "all engine running... LIFT-OFF!";
+var apolloCountdownMessage = 'all engine running... LIFT-OFF!';
 var countdown = 8;
-
+for (let i = 8; i > 0; i--) {
+	console.log(i);
+}
 console.log(apolloCountdownMessage);
