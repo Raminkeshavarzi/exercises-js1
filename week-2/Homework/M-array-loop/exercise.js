3c4c1e723eb832d5f32212f9b678d4157130928a
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-08-19 16:45:14 +0200

    added the codes

--- week-2/Homework/M-array-loop/exercise.js
+++ week-2/Homework/M-array-loop/exercise.js
@@ -3,13 +3,14 @@
 	https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
 */
 
+const daysOfWeek = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ];
 
-const daysOfWeek = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
+const startWithT = (arr) => {
+	for (let i in arr) {
+		if (arr[i].startsWith('T')) {
+			console.log(arr[i]);
+		}
+	}
+};
+
+console.log(startWithT(daysOfWeek));
