343d49d09fe24763310bc164069f9d197e4370aa
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-08-25 19:39:10 +0200

    solve the homework

---
+++ week-3/InClass/exercise-C.js
@@ -0,0 +1,10 @@
+function abracaFunction(yourFunc) {
+	console.log("I am abracaFunction! Watch as I mutate an array of strings to your heart's content!");
+	const abracaArray = [ 'James', 'Elamin', 'Ismael', 'Sanyia', 'Chris', 'Antigoni' ];
+	const abracaOutput = abracaArray.map((idx) => idx.toUpperCase());
+	return abracaOutput;
+}
+
+// abracaArray.map(idx => idx.toUpperCase());
+
+console.log(abracaFunction(this.abracaOutput));
