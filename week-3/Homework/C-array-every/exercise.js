33575ea48389e262aa655b8f49ca9f4387035d41
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-08-27 11:19:38 +0200

    solve the code

--- week-3/Homework/C-array-every/exercise.js
+++ week-3/Homework/C-array-every/exercise.js
@@ -2,17 +2,31 @@
    This program should check if the array `group` contains only students
 */
 
-var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
-var group = ["Austine", "Dany", "Swathi", "Daniel"];
+var students = [ 'Omar', 'Austine', 'Dany', 'Swathi', 'Lesley', 'Rukmini' ];
+var group = [ 'Austine', 'Dany', 'Swathi', 'Daniel' ];
 
-var groupIsOnlyStudents; // complete this statement
+const groupIsOnlyStudents = group.every(
+	(idx) =>
+		group.includes(students)
+			? console.log('The group contains only students')
+			: console.log('The group does not contain only students')
+);
 
-if (groupIsOnlyStudents) {
-  console.log("The group contains only students");
-} else {
-  console.log("The group does not contain only students");
-}
+//  or using nested for loop
 
+const checkIs = (arr1, arr2) => {
+	for (let i = 0; i < arr1.length; i++) {
+		for (let j = 0; j < arr2.length; j++) {
+			if (!arr1.includes(arr2[j])) {
+				return 'The group contains only students';
+			} else {
+				return 'The group does not contain only students';
+			}
+		}
+	}
+};
+
+console.log(checkIs(students, group));
 /* EXPECTED RESULT */
 
 // The group does not contain only students
