c13076709a56797a4ed06864699b1c01fb1b5488
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-08-27 12:10:05 +0200

    solve the code

--- week-3/Homework/D-array-filter/exercise.js
+++ week-3/Homework/D-array-filter/exercise.js
@@ -6,17 +6,18 @@
   - Do not edit any of the existing code
 */
 
-var pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
+var pairsByIndexRaw = [ [ 0, 3 ], [ 1, 2 ], [ 2, 1 ], null, [ 1 ], false, 'whoops' ];
 
-var pairsByIndex; // Complete this statement
+var pairsByIndex = pairsByIndexRaw.filter(Boolean);
+pairsByIndex = pairsByIndex.filter((val) => !(typeof val === 'string'));
 
-var students = ["Islam", "Lesley", "Harun", "Rukmini"];
-var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
+var students = [ 'Islam', 'Lesley', 'Harun', 'Rukmini' ];
+var mentors = [ 'Daniel', 'Irina', 'Mozafar', 'Luke' ];
 
 var pairs = pairsByIndex.map(function(indexes) {
-  var student = students[indexes[0]];
-  var mentor = mentors[indexes[1]];
-  return [student, mentor];
+	var student = students[indexes[0]];
+	var mentor = mentors[indexes[1]];
+	return [ student, mentor ];
 });
 
 console.log(pairs);
